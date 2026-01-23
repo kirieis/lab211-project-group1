793213a23cce49caa99eb415b3c0cb7a7d41a6c4
kirieis <nguyenansuper123@gmail.com>
2026-01-23 21:02:34 +0700

    update util
    connection check whether it work or not

---
+++ src/core_app/util/DBConnection.java
@@ -0,0 +1,28 @@
+package core_app.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:sqlserver://localhost:1433;databaseName=PharmacyDB;encrypt=false";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "123456";
+
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("❌ Cannot load SQL Server Driver", e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("❌ DB Connection failed", e);
+        }
+    }
+}
